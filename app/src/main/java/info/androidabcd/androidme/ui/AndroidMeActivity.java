b327959fd087ef1690e925c85c1689255f403322
Gaurab Kumar <progaurab@gmail.com>
2018-11-05 22:44:31 +0530

    Initial commit

---
+++ app/src/main/java/info/androidabcd/androidme/ui/AndroidMeActivity.java
@@ -0,0 +1,15 @@
+package info.androidabcd.androidme.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.androidabcd.androidme.R;
+
+public class AndroidMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_me);
+    }
+}

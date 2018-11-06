2b4cec9e029671cc98ce3cacb5fa93517d438f6a
Gaurab Kumar <progaurab@gmail.com>
2018-11-06 09:05:36 +0530

    Created Full Android Single View.

---
+++ app/src/main/java/info/androidabcd/androidme/ui/BodyPartFragment.java
@@ -0,0 +1,67 @@
+package info.androidabcd.androidme.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+import info.androidabcd.androidme.R;
+import info.androidabcd.androidme.data.AndroidImageAssets;
+
+public class BodyPartFragment extends Fragment {
+
+    // Tag for logging
+    private static final String TAG = "BodyPartFragment";
+
+    // Variables to store a list of image resources and the index of the image that this fragment displays
+    private List<Integer> mImageIds;
+    private int mListIndex;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the fragment
+     */
+    public BodyPartFragment() {
+    }
+
+    /**
+     * Inflates the fragment layout file and sets the correct resource for the image to display
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the Android-Me fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        // Get a reference to the ImageView in the fragment layout
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        // If a list of image ids exists, set the image resource to the correct item in that list
+        // Otherwise, create a Log statement that indicates that the list was not found
+        if(mImageIds != null){
+            // Set the image resource to the list item at the stored index
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        } else {
+            Log.v(TAG, "This fragment has a null list of image id's");
+        }
+
+        // Return the rootView
+        return rootView;
+    }
+
+    // Setter methods for keeping track of the list images this fragment can display and which image
+    // in the list is currently being displayed
+
+    public void setImageIds(List<Integer> imageIds) {
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex(int index) {
+        mListIndex = index;
+    }
+
+}
